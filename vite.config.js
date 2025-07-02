0de82a58908d83c6c041e044fdb86c73192260d4
Vinith Rexton P <vinith.p@bounteous.com>
2025-07-02 18:01:49 +0530

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/toshiba-commerce/', // 👈 this is required
   plugins: [react()],
 })
